196b5a281bbf2e26d23d006303665dd51c31d012
iroro1 <ojigboleo@gmail.com>
2020-02-10 13:47:56 +0100

    changedd to es 6 and fix icon on delete button

--- quote/script.js
+++ quote/script.js
@@ -5,17 +5,25 @@ let tweet = document.querySelector("#tweet-quote");
 
 const api = () => {
   let txt;
+  let data;
   fetch(`https://thesimpsonsquoteapi.glitch.me/quotes`)
     .then(res => res.json())
     .then(data => {
-      text.textContent = data[0].quote;
-      author.textContent = data[0].character;
-      txt = data[0].quote;
-      tweet.setAttribute(
-        "href",
-        `https://twitter.com/intent/tweet?text=${txt}`
-      );
+      data;
     });
+
+  if (data) {
+    text.textContent = data[0].quote;
+    author.textContent = data[0].character;
+    txt = data[0].quote;
+    tweet.setAttribute("href", `https://twitter.com/intent/tweet?text=${txt}`);
+  } else {
+    text.textContent =
+      "OOps sorry. \r The Server May be Down. Plese Try Again Later ...";
+    author.textContent = "Server Error";
+    txt = "The Server May be Down. Plese Try Again Later";
+    tweet.setAttribute("href", `https://twitter.com/intent/tweet?text=${txt}`);
+  }
 };
 window.addEventListener("load", api);
 nextBtn.addEventListener("click", api);
