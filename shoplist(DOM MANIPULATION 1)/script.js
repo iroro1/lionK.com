d9d90a8d8cea3ced1999f4fb6bcaf5909d09625c
iroro1 <ojigboleo@gmail.com>
2020-02-10 13:10:26 +0100

    Fixed local storage on the shopping list

--- shoplist(DOM MANIPULATION 1)/script.js
+++ shoplist(DOM MANIPULATION 1)/script.js
@@ -3,65 +3,81 @@ var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 let lis = document.querySelectorAll("li");
 let rmbutton = document.querySelectorAll(".remove");
+let p = document.querySelector("#first");
 
+// load local Storage
+function loadList(e) {
+  let list = localStorage.getItem("items");
+  if (list === null || list === "[]") {
+    console.log("InValid");
+    localStorage.setItem("items", "[]");
+    p.textContent = "EMPTY LIST";
+    p.setAttribute("class", "text-danger");
+  } else {
+    p.textContent = "Get it done today";
+    p.setAttribute("class", "text-success");
+    JSON.parse(list).forEach(e => createListElement(e, "create"));
+  }
+}
+
+function listInValid(list) {
+  if (list === null && list === undefined && list === "" && list.length === 0) {
+    return 0;
+  } else {
+    return 1;
+  }
+}
 function inputLength() {
   return input.value.length;
 }
-
-function createListElement() {
-  var li = document.createElement("li");
+function makeIt(val) {
+  let li = document.createElement("li");
   let dbtn = document.createElement("button");
+  let i = document.createElement("i");
+  // i.setAttribute("class", "fas fa-trash-alt");
   dbtn.setAttribute("class", "remove btn");
-  dbtn.textContent = "x";
-
-  li.appendChild(document.createTextNode(input.value));
+  dbtn.appendChild(i);
+  li.appendChild(document.createTextNode(val));
   li.appendChild(dbtn);
   ul.appendChild(li);
   input.value = "";
 }
-// load local Storage
-let state ={
-  items=[]
-};
-function loadList() {
-  if (localStorage.getItem("items") === "") {
-    return "The Shopping List Is Empty.";
+function createListElement(val, wtd) {
+  if (wtd === "create") {
+    makeIt(val);
   } else {
-    state.items = localStorage.getItem("items");
-    console.log(state.items);
+    let list = JSON.parse(localStorage.getItem("items"));
+    localStorage.setItem("items", JSON.stringify([...list, val]));
+    makeIt(val);
   }
 }
-// items = loadList();
-console.log(items);
-
-function addListAfterClick() {
+function addListAfterClick(e) {
   if (inputLength() > 0) {
     // local storage
-    localStorage.setItem("items", [...items, input.value]);
-    createListElement();
+    createListElement(input.value);
   }
 }
-
-function addListAfterKeypress(event) {
+function addListAfterKeypress(event, v) {
   if (inputLength() > 0 && event.keyCode === 13) {
-    createListElement();
+    createListElement(input.value);
   }
 }
-
 function lisManipulation(e) {
-  //   console.log(e.target.textContent);
   if (e.target.className !== "remove btn") {
     return e.target.classList.toggle("done"); //Mark item
   } else {
     // delete item
     e.target.parentElement.remove();
+    let list = localStorage.getItem("items");
+    list = JSON.parse(list);
+    let val = e.target.parentElement.textContent;
+    console.log(val);
+    let nlist = list.filter(e => e !== val);
+    localStorage.setItem("items", JSON.stringify(nlist));
   }
 }
 
-button.addEventListener("click", addListAfterClick);
-
-input.addEventListener("keypress", addListAfterKeypress);
-
-ul.addEventListener("click", lisManipulation);
-
 window.addEventListener("load", loadList);
+button.addEventListener("click", addListAfterClick);
+input.addEventListener("keypress", addListAfterKeypress);
+ul.addEventListener("click", lisManipulation);
