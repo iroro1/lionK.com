e84339a1d180866c5e70126573fe91aed7e03df6
iroro1 <ojigboleo@gmail.com>
2020-02-11 12:09:21 +0100

    Added spiner to quotes gen and footer to all projects

--- quote/script.js
+++ quote/script.js
@@ -10,21 +10,21 @@ const rand = d => {
 };
 
 const u = ["red", "blue", "green", "crimson", "maroon"];
-const i = [
-  "linear-gradient(to right, rgb(44, 88, 0), rgb(1, 178, 225));",
-  "linear-gradient(to right, rgba(239, 38, 65, 0.95), rgba(194, 125, 117, 0.02));",
-  "linear-gradient(to right, rgb(175, 253, 10), rgba(181, 251, 103, 0.88));",
-  "linear-gradient(to right, rgba(201, 239, 132, 0.19), rgba(5, 13, 200, 0.87));",
-  "linear-gradient(to right, rgba(25, 175, 255, 0.78), rgb(241, 72, 140));",
-  "linear-gradient(to right, rgba(200, 181, 12, 0.94), rgb(214, 73, 63));",
-  "https://images.unsplash.com/photo-1446457292373-a8e0bb763287?ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60",
-  "https://images.unsplash.com/photo-1508349937151-22b68b72d5b1?ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60",
+// const i = [
+//   "linear-gradient(to right, rgb(44, 88, 0), rgb(1, 178, 225));",
+//   "linear-gradient(to right, rgba(239, 38, 65, 0.95), rgba(194, 125, 117, 0.02));",
+//   "linear-gradient(to right, rgb(175, 253, 10), rgba(181, 251, 103, 0.88));",
+//   "linear-gradient(to right, rgba(201, 239, 132, 0.19), rgba(5, 13, 200, 0.87));",
+//   "linear-gradient(to right, rgba(25, 175, 255, 0.78), rgb(241, 72, 140));",
+//   "linear-gradient(to right, rgba(200, 181, 12, 0.94), rgb(214, 73, 63));",
+//   "https://images.unsplash.com/photo-1446457292373-a8e0bb763287?ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60",
+//   "https://images.unsplash.com/photo-1508349937151-22b68b72d5b1?ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60",
 
-  "https://images.unsplash.com/photo-1519329475180-feddc4230aa6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-  "https://images.unsplash.com/photo-1513436158476-868ee8782fdd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-  "https://images.unsplash.com/photo-1534794067417-dce2680b2bb9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-  "https://images.unsplash.com/photo-1474511320723-9a56873867b5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-];
+//   "https://images.unsplash.com/photo-1519329475180-feddc4230aa6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+//   "https://images.unsplash.com/photo-1513436158476-868ee8782fdd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+//   "https://images.unsplash.com/photo-1534794067417-dce2680b2bb9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+//   "https://images.unsplash.com/photo-1474511320723-9a56873867b5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+// ];
 
 body.style.height = "100vh";
 const changeBg = () => {
@@ -57,7 +57,7 @@ const api = async () => {
     res.json()
   );
   changeColor();
-  changeBg();
+  // changeBg();
   if (data) {
     let i = rand(data);
     text.textContent = data[i].text;
